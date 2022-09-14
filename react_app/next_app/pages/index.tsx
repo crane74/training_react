2bc60c0b839e4290b0c76cf39e08550f63cbdb37
crane74 <a18.e73k@g.chuo-u.ac.jp>
2022-09-14 12:46:47 +0900

    chore: Heroicons,HeadlessUIのインストール
        Heroicons: アイコンフォント
        HeadlessUI: UIコンポーネント

--- react_app/next_app/pages/index.tsx
+++ react_app/next_app/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Link from "next/link";
+import DropdownMenu from "@/components/elements/DropdownMenu";
 
 const Home: NextPage = () => {
   return (
@@ -7,6 +8,7 @@ const Home: NextPage = () => {
       <h1 className="text-3xl font-bold underline bg-red-500 hover:bg-orange-500">
         Hello world!
       </h1>
+      <DropdownMenu />
       <Link href="/sample">
         <a>sample</a>
       </Link>
