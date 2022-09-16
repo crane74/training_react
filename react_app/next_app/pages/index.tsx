a245bf51dab6a86141a734be762d78aa8db80c26
crane74 <a18.e73k@g.chuo-u.ac.jp>
2022-09-16 21:42:16 +0900

    chore: daisyUIの導入

--- react_app/next_app/pages/index.tsx
+++ react_app/next_app/pages/index.tsx
@@ -15,6 +15,25 @@ const Home: NextPage = () => {
           <a>sample</a>
         </Link>
       </div>
+      <div className="drawer">
+        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content">
+          <label htmlFor="my-drawer" className="btn btn-primary drawer-button">
+            Open drawer
+          </label>
+        </div>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer" className="drawer-overlay"></label>
+          <ul className="menu w-80 overflow-y-auto bg-base-100 p-4 text-base-content">
+            <li>
+              <a>Sidebar Item 1</a>
+            </li>
+            <li>
+              <a>Sidebar Item 2</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </>
   );
 };
--- react_app/next_app/tailwind.config.js
+++ react_app/next_app/tailwind.config.js
@@ -14,5 +14,6 @@ module.exports = {
     require("@tailwindcss/line-clamp"),
     require("@tailwindcss/forms"),
     require("@tailwindcss/aspect-ratio"),
+    require("daisyui")
   ],
 };
