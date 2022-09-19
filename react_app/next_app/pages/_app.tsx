2bb4190e521ca400ea97918b7d831fde9bd3e601
crane74 <a18.e73k@g.chuo-u.ac.jp>
2022-09-19 21:00:52 +0900

    feat: フッターを最下部に固定

--- react_app/next_app/pages/_app.tsx
+++ react_app/next_app/pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Layout } from "@/components/layouts/Layout";
 import { AnimatePresence } from "framer-motion";
+import Div100vh from "react-div-100vh";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -10,7 +11,9 @@ function MyApp({ Component, pageProps }: AppProps) {
       onExitComplete={() => window.scrollTo(0, 0)}
     >
       <Layout>
-        <Component {...pageProps} />
+        <Div100vh>
+          <Component {...pageProps} />
+        </Div100vh>
       </Layout>
     </AnimatePresence>
   );
